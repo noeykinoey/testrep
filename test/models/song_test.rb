8867c98de4c281cbddb9b42ee2e3259a14dea230
noeykinoey <noeykinoey@gmail.com>
2020-08-04 15:20:15 +1000

    Improving song tests

--- test/models/song_test.rb
+++ test/models/song_test.rb
@@ -14,6 +14,11 @@ class SongTest < ActiveSupport::TestCase
     song.valid?
     assert_empty song.errors[:title]
 
+  test "invalid if less than 2 letters" do
+    song = Song.new(title: "N")
+    song.valid?
+    assert_equal 
+
   end
 
 end
